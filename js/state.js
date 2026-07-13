cd1a62e51557a4561dbf4f6c229dab130d3b9284
lebob18-ux <lebob18@gmail.com>
2026-07-13 10:07:09 +0200

    Add majAffichageSensP call on window load

--- js/state.js
+++ js/state.js
@@ -2,7 +2,7 @@
    ÉTAT GLOBAL & INITIALISATION
    ============================================================ */
 /* Initialisation */
-window.onload = () => {initChantiers();  calculer();};
+window.onload = () => {initChantiers();  calculer(); majAffichageSensP("APRES");};
 let choixExtensionA = "centre";
 let modalOuvert = false;
 let largeurEchantillon = 0;
