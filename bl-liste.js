dc79b05500fea191caf7f0e0cb8d912df1e866c6
lebob18-ux <lebob18@gmail.com>
2026-08-09 12:42:08 +0200

    Add new concrete grades to LISTE_BL

--- bl-liste.js
+++ bl-liste.js
@@ -5,6 +5,8 @@
    ============================================================ */
 const LISTE_BL = [
   "C35/45 330 D20 S3 XC3 (IN34) SRPM CL0,65",
+   "C35/50 330 D20 S3 XC3 (IN34) SRPM CL0,65",
+   "C35/60 330 D20 S3 XC3 (IN34) SRPM CL0,65",
 
   /* Ajoute tes vrais numéros BL ici */
 ];
