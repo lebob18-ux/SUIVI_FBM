a3bdb6f16518e644253b2a330939a4bf511bc4d1
lebob18-ux <lebob18@gmail.com>
2026-08-15 09:08:12 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// Enregistrement du Service Worker pour rendre l'outil FBM installable et hors-ligne
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(() => console.log("Service Worker FBM enregistré !"))
-      .catch((err) => console.log("Erreur Service Worker FBM :", err));
-  });
-}
