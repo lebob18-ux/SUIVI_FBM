90cd62f447494e82a7f64fdb58ac42e6be47eaa4
lebob18-ux <lebob18@gmail.com>
2026-09-24 16:40:52 +0200

    Add files via upload

---
+++ partage.js
@@ -0,0 +1,7 @@
+/* ============================================================
+   PAD DE SIGNATURE (souris + tactile)
+   ============================================================ */
+let sigCtx = null;
+let sigDessin = false;
+let sigVide = true;
+
